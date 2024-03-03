7ce7d57e6e431dcb5b20780d218c0b04b6ddb326
wilwe <wilwe036@gmail.com>
2024-03-03 19:03:51 +0100

    si encante

--- src/main.js
+++ src/main.js
@@ -1,31 +1,76 @@
 import kaboom from "kaboom"
 
+
 const k = kaboom()
 k.loadSprite('bean', 'sprites/bean.png')
+/*const levels = [
+    [
+        "##########",
+        "#        #",
+        "#        #",
+        "#        #",
+        "#        #",
+        "#        #",
+        "#        #",
+        "#        #",
+        "#        #",
+        "##########",
+    ],
+]
+const level = addLevel(levels[levelIdx], {
+    tileWidth: 64,
+    tileHeight:64,
+    pos: vec2(64,64),
+    tiles: {
+        "#": () => [
+            sprite("bean"),
+            area(),
+            anchor("center"),
+        ],
+    },
+})*/
 
 const speed = 300
+let cul = 0
+let ters = 3
+let acqd = 0
 
 const player = add([
     sprite("bean"),
     area(),
     pos(100,200),
     anchor("center"),
+    "player",
 ])
-const BULLET_SPEED = 1200
-function spawnBullet(p, d) {
-	add([
-		rect(12, 48),
-		area(),
-		pos(p),
-		anchor("center"),
-		color(127, 127, 255),
-		outline(4),
-		move(d, BULLET_SPEED),
-		offscreen({ destroy: true }),
-		// strings here means a tag
-		"bullet",
-	])
+//const playera = level.get("player")[0]
+let BULLET_SPEED = 600
+const spawnBullet = (p, d) => {
+    if (cul == 0) {
+	    add([
+		    circle(12),
+		    area(),
+		    pos(p),
+		    anchor("center"),
+		    color(255, 0, 0),
+		    outline(4),
+		    move(d, BULLET_SPEED),
+		    offscreen({ destroy: true }),
+		    // strings here means a tag
+		    "bullet",
+	    ]);
+	    cul = ters;
+	}
 }
+loop(0.2, () => {
+    if (cul > 0){
+        cul -= 1
+    }
+})
+loop(0.2, () => {
+    if ( acqd > 0){
+        acqd -= 1
+    }
+})
 onKeyDown("a", () => {
     player.move(-speed, 0)
     player.angle = 90
@@ -42,9 +87,19 @@ onKeyDown("w", () => {
     player.move(0,-speed)
     player.angle = 180
 })
+onKeyDown("space", () => {
+    if ( acqd == 0 ){
+        acqd = 10
+        ters = 1
+        wait(0.4, () => {
+            ters = 6
+        })
+    }
+})
 onKeyDown("left", () => {
     player.angle = 90
     spawnBullet(player.pos.sub(-16, 0),LEFT)
+    cul 
 })
 onKeyDown("right", () => {
     player.angle = 270
@@ -58,4 +113,3 @@ onKeyDown("down", () => {
     player.angle = 0
     spawnBullet(player.pos.sub(0, -16),DOWN)
 })
-k.onClick(() => k.addKaboom(k.mousePos()))
